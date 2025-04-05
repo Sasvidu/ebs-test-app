79b241792f5324ceffc3c804a053cac8c9c65abb
Sasvidu Abesinghe <66816946+Sasvidu@users.noreply.github.com>
2025-04-05 18:14:37 +0530

    fix: error

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,37 +3,38 @@ import { PrismaService } from '../prisma.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Todo } from './entities/todo.entity';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class TodoService {
   constructor(private prisma: PrismaService) {}
 
   async create(createTodoDto: CreateTodoDto): Promise<Todo> {
-    return this.prisma.todo.create({
+    return await this.prisma.todo.create({
       data: createTodoDto,
-    });
+    }) as Todo;
   }
 
   async findAll(): Promise<Todo[]> {
-    return this.prisma.todo.findMany();
+    return await this.prisma.todo.findMany() as Todo[];
   }
 
   async findOne(id: number): Promise<Todo | null> {
-    return this.prisma.todo.findUnique({
+    return await this.prisma.todo.findUnique({
       where: { id },
-    });
+    }) as Todo | null;
   }
 
   async update(id: number, updateTodoDto: UpdateTodoDto): Promise<Todo> {
-    return this.prisma.todo.update({
+    return await this.prisma.todo.update({
       where: { id },
       data: updateTodoDto,
-    });
+    }) as Todo;
   }
 
   async remove(id: number): Promise<Todo> {
-    return this.prisma.todo.delete({
+    return await this.prisma.todo.delete({
       where: { id },
-    });
+    }) as Todo;
   }
 }
